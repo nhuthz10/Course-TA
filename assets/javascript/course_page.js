ab6973333b526e338972636afae0592b9a0fbdd0
nhuthz10 <nhuthz10@gmail.com>
2023-04-06 22:57:38 +0700

    add table_ratings,comment in course_page

--- assets/javascript/course_page.js
+++ assets/javascript/course_page.js
@@ -5,7 +5,11 @@ const listCourseEnglish = document.querySelector('.js-list-course__english');
 const courseEnglishContent = document.querySelector('.js-course-english__content');
 const stageEnglishStart = document.querySelector('.js-stage-english__start');
 const courseEnglishStart = document.querySelector('.js-course-english__start');
-
+const modalCommentEnglish = document.querySelector('.modal-course-english__comment')
+const bntCommnet = document.querySelector('.course-english__comment-btn');
+const bntLoseComment = document.querySelector('.modal-course-english__comment-close');
+const modalContainerCommnet = document.querySelector('.modal-course-english__comment-container');
+const ListIconComment = document.querySelectorAll('.modal-course-english__comment-item-icon');
 // listCourse
 imgEnglish.addEventListener('click', function() {
     listCourseContent.style.display = "none";
@@ -15,4 +19,29 @@ imgEnglish.addEventListener('click', function() {
 stageEnglishStart.addEventListener('click', function() {
     courseEnglishContent.style.display = "none";
     courseEnglishStart.style.display = "block";
+})
+
+// modal
+bntCommnet.addEventListener('click', function() {
+    modalCommentEnglish.style.display = 'flex';
+})
+bntLoseComment.addEventListener('click', function() {
+    modalCommentEnglish.style.display = 'none';
+})
+modalCommentEnglish.addEventListener('click', function() {
+    modalCommentEnglish.style.display = 'none';
+})
+modalContainerCommnet.addEventListener('click', function(event) {
+    event.stopPropagation();
+})
+
+// choose icon
+ListIconComment.forEach(icon => {
+    icon.addEventListener('click', function() {
+        ListIconComment.forEach(icon2 => {
+            if (icon != icon2)
+                icon2.classList.remove('modal-course-english__comment-item-icon--chose');
+        })
+        icon.classList.toggle('modal-course-english__comment-item-icon--chose');
+    })
 })
